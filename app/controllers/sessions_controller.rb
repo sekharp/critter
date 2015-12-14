ca2c14e6d692ef2e7d7057d1a872593dab64e8ab
Sekhar Paladugu <sekharp@gmail.com>
2015-12-14 14:05:15 -0700

    initial apicurious project commit

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+  def create
+    if user = User.from_omniauth(request.env["omniauth.auth"])
+      session[:user_id] = user.id
+    end
+    redirect_to root_path
+  end
+
+  def destroy
+    session.clear
+    flash[:logout] = "Goodbye!"
+    redirect_to root_path
+  end
+end
